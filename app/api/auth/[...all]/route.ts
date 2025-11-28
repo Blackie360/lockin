e29268a2aabfd86bad17eb62caad4f499b0af8b7
blackie360 <felixkent360@gmail.com>
2025-11-28 23:55:58 +0300

    Remove commented-out line regarding social provider environment variables in auth route

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,5 +1,4 @@
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
-// Added social provider env vars (Google, GitHub credentials)
 export const { POST, GET } = toNextJsHandler(auth);
